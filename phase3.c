3369a78ea9c693de691343d84ed321e9278e03d7
chris <cray0101@gmail.com>
2016-04-13 01:38:07 -0700

    //P3_Startup should call Sys_VmDestroy if P4_Startup returns

--- phase3.c
+++ phase3.c
@@ -15,7 +15,20 @@
 
 
 
+typedef struct spawn_wrapper {
+	int (*f)(void *);
+	void *arg;
+} spawn_wrapper;
 
+
+int P4_Startup_Spawn_Wrapper(void* arg) {
+	spawn_wrapper *s = malloc(sizeof(spawn_wrapper));
+	s = (spawn_wrapper *) arg;
+	int i = s->f(s->arg);
+	//P3_Startup should call Sys_VmDestroy if P4_Startup returns
+	Sys_VmDestroy();
+	return i;
+}
 
 
 
@@ -23,10 +36,18 @@ int P3_Startup(void *arg){
 	int p4_pid;
 	int status;
 
+
+
+
+
 	/*
 	 *  Fork P4_Startup process. Do this after all other setup code.
 	 */
-	p4_pid = P2_Spawn("P4_Startup", P4_Startup, NULL, 4 * USLOSS_MIN_STACK, 3);
+	//P3_Startup should call Sys_VmDestroy if P4_Startup returns
+	spawn_wrapper *helper = malloc(sizeof(spawn_wrapper));
+			helper->f = P4_Startup;
+			helper->arg = NULL;
+	p4_pid = P2_Spawn("P4_Startup", &P4_Startup_Spawn_Wrapper, (void *) helper, 4 * USLOSS_MIN_STACK, 3);
 	p4_pid = P2_Wait(&status);
 
 	return 0;
