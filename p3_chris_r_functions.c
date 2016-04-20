da62a81f251b102045249fc8bedbad50429381a8
chris <cray0101@gmail.com>
2016-04-20 03:48:58 -0700

    chris ray is a day late and a dollar short v_V

--- p3_chris_r_functions.c
+++ p3_chris_r_functions.c
@@ -23,7 +23,7 @@ void P3_Quit(pid)
 
 	
 	P1_P(process_sem);
-	if (IsVmInitialized == TRUE && processes[pid].numPages > 0 && processes[pid].pageTable != NULL && P1_GetState(pid) != PROC_STATE_QUIT) { // do nothing if  VM system is uninitialized
+	if (IsVmInitialized == TRUE && processes[pid].numPages > 0 && processes[pid].pageTable != NULL ) { // do nothing if  VM system is uninitialized
 
 		CheckMode();
 		CheckPid(pid);
@@ -42,6 +42,7 @@ void P3_Quit(pid)
 			frames_list[processes[pid].pageTable[i].frame] = UNUSED;
 			processes[pid].pageTable[i].frame = -1;
 			processes[pid].pageTable[i].state = UNUSED;
+			USLOSS_MmuUnmap(TAG,i);
 		}
 
 		/* Clean up the page table. */
